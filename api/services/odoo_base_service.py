47dcebc2d5fae3f051b57ae51f84e40589f220d8
El Pelotazo Developer <user@example.com>
2025-06-25 12:53:50 +0200

    git commit -m "FIX: Corrige conexión Odoo en servicios refactorizados y base. Actualiza IP a 172.18.0.5. [2606 EXITO]" --no-edit FIX: Corrige conexión Odoo en servicios refactorizados y base. Soluciona error 500 productos/proveedores. Actualiza IP a 172.18.0.5. [2606 EXITO]

--- api/services/odoo_base_service.py
+++ api/services/odoo_base_service.py
@@ -11,7 +11,7 @@ class OdooBaseService:
         self._common = None
         self._models = None
         self._uid = None
-        self._url = 'http://172.18.0.4:8069'
+        self._url = 'http://172.18.0.5:8069'  # IP correcta del contenedor Odoo - Actualizado 2025-06-25
     
     def _get_connection(self):
         """Establece conexión con Odoo"""
